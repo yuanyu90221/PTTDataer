3421f5c1755b30285d19868d2e4e5982fdfafcd1
lin sam <610411102@ems.ndhu.edu.tw>
2018-04-12 19:49:55 +0800

    Add files via upload

---
+++ upload_data_to_mysql.r
@@ -0,0 +1,65 @@
+
+
+library(RODBC)
+library(dbConnect)
+library(DBI)
+library(gWidgets)
+library(RMySQL)
+library(data.table)
+
+creat_sql_file = function(dataset_name= 'test7'){
+  # connect mysql
+  connect = dbConnect(MySQL(), 
+                      dbname = "guest_dataset",# dataset name
+                      username = "guest", 
+                      password = "123",   
+                      host = "114.34.138.146")
+  #dataset_name = 'test7'
+  sql_string = paste('create table ' , dataset_name , '( i text(100),date text(100) )',sep='' )
+  dbGetQuery(connect, sql_string) # db Get Query
+
+  # 加 PRIMARY KEY 
+  sql_string = paste('ALTER TABLE `',dataset_name,
+                     '` ADD id BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY;',
+                     sep='')
+  dbGetQuery(connect, sql_string) # db Get Query
+  dbDisconnect(connect)# close sql connect
+}
+
+
+input_data_to_sql = function(dataset_name= 'test7',data){
+  # connect mysql
+  connect = dbConnect(MySQL(), 
+                      dbname = "guest_dataset",# dataset name
+                      username = "guest", 
+                      password = "123",   
+                      host = "114.34.138.146")
+  
+  dbWriteTable(connect, dataset_name,data, append = TRUE, 
+               overwrite=FALSE, row.name=FALSE) 
+  
+  dbDisconnect(connect)# close sql connect
+}
+
+# creat sql file/table
+creat_sql_file(dataset_name = 'test8')
+
+i = c(1:5)
+date = paste('2017-03-0' , i,sep='')
+data = data.table(i,date)
+
+input_data_to_sql(dataset_name='test8',data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
