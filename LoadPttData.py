15f79b5e1227b70b82e3fb2c04f3fab8a29142ae
f496328mm <f496328mm@gmail.com>
2018-10-28 13:37:13 +0800

    re init

---
+++ LoadPttData.py
@@ -0,0 +1,97 @@
+
+import pymysql
+import pandas as pd
+import numpy as np
+
+#host = '114.32.60.100'
+host = 'localhost'
+user = 'guest'
+password = '123'
+
+#--------------------------------------------------------
+
+def execute_sql2(sql_text):
+    
+    conn = ( pymysql.connect(host = host,# SQL IP
+                     port = 3306,
+                     user = user,# 帳號
+                     password = password,# 密碼
+                     database = 'ptt_data1.0',  # 資料庫名稱
+                     charset="utf8") )   #  編碼
+                             
+    cursor = conn.cursor()    
+    # sql_text = "SELECT * FROM `_0050_TW` ORDER BY `Date` DESC LIMIT 1"
+    try:   
+        cursor.execute(sql_text)
+        data = cursor.fetchall()
+        conn.close()
+        return data
+    except:
+        conn.close()
+        return ''
+
+class main:
+    #---------------------------------------------------------------    
+    def get_col_name(self,database,data_name):
+       
+        tem_col_name = execute_sql2(
+                sql_text = 'SHOW columns FROM '+ data_name )
+    
+        col_name = [ tem[0] for tem in tem_col_name ]
+        col_name.remove('id')    
+        self.col_name = col_name
+    
+    def load(self,data_name):
+        
+        self.get_col_name('ptt_data1.0',data_name)
+    
+        data = pd.DataFrame()
+        for j in range(len(self.col_name)):
+            #print(j)
+            col = self.col_name[j]
+            text = 'select ' + col + ' from ' + data_name
+            
+            tem = execute_sql2(
+                sql_text = text)
+            
+            if col=='Date':
+                tem = [np.datetime64(x[0]) for x in tem]
+                tem = pd.DataFrame(tem)
+                data[col] = tem.loc[:,0]
+            else:
+                tem = np.concatenate(tem, axis=0)
+                tem = pd.DataFrame(tem)
+                data[col] = tem[0]
+                
+        return data
+
+def LoadPttData(data_name):
+    self = main()
+    data = self.load(data_name)
+    
+    return data
+
+#---------------------------------------------------------
+'''
+tem = execute_sql2(host,user,password,'ptt_data1.0','show tables')
+all_data_table_name = np.concatenate(tem, axis=0)
+# test
+tem = execute_sql2(host,user,password,'ptt_data1.0','show tables')
+all_data_table_name = np.concatenate(tem, axis=0)
+all_data_table_name
+
+#---------------------------------------------------------
+
+import sys
+sys.path.append('/home/linsam/github')
+from Crawler_and_Share.LoadPttData import LoadPttData
+
+data = LoadPttData('job')
+data['title']
+
+
+'''
+
+
+
+
