d31776e01bc7312ca9aaaa504eb2f6a0ff221271
Lin Sam <sam.lin@tripsaas.com>
2019-01-04 23:00:36 +0800

    fix demo.py

--- demo.py
+++ demo.py
@@ -3,11 +3,9 @@
 
 from PTTData import Load as PTT
 
-print(' get all ptt table name ' )
 PTT_data_list = PTT.LoadDataList()
 print(PTT_data_list[:5])
 
-print('load data')
 data = PTT.LoadData(table = 'job',date = '2018-12-10',select = 'title')
 print(data[:5])
 
