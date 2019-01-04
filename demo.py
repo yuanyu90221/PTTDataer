8b40cd27728cb05c6b7482ec5f11707b244825e1
Lin Sam <sam.lin@tripsaas.com>
2019-01-04 23:39:51 +0800

    change demo.py date to today

--- demo.py
+++ demo.py
@@ -2,11 +2,17 @@
 
 
 from PTTData import Load as PTT
+import datetime
+
+date = str( datetime.datetime.now().date() )
 
 PTT_data_list = PTT.LoadDataList()
 print(PTT_data_list[:5])
 
-data = PTT.LoadData(table = 'job',date = '2018-12-10',select = 'title')
+data = PTT.LoadData(table = 'job',date = date,select = 'title')
+print(data[:5])
+
+data = PTT.LoadData(table = 'AdvEduUK',date = date,select = 'article')
 print(data[:5])
 
 
