8fe9d8ec20e3d672ca0538310a2e4142819dd08f
yuanyu90221 <yuanyu90221@gmail.com>
2019-02-15 01:06:07 +0800

    change-test-case

--- demo.py
+++ demo.py
@@ -4,15 +4,15 @@
 from PTTData import Load as PTT
 import datetime
 
-date = str( datetime.datetime.now().date() )
+date = str( datetime.datetime.now().date() - datetime.timedelta(10) )
 
 PTT_data_list = PTT.LoadDataList()
-print(PTT_data_list[:5])
+print(PTT_data_list[:6])
 
 data = PTT.LoadData(table = 'job',date = date,select = 'title')
 print(data[:5])
 
-data = PTT.LoadData(table = 'AdvEduUK',date = date,select = 'article')
+data = PTT.LoadData(table = 'Gemini',date = date,select = 'response')
 print(data[:5])
 
 
